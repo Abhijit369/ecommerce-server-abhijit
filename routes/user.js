751e414c3bf4065f48d5243f7a4f9ef7b0a8d5b3
Abhijit369 <abhijitrath369@gmail.com>
2022-09-29 22:34:20 +0530

    order

--- routes/user.js
+++ routes/user.js
@@ -73,7 +73,7 @@ userRoute.post("/api/save-user-address", auth, async (req, res) => {
 });
 
 // order product
-userRoute.post("/api/orders", auth, async (req, res) => {
+userRoute.post("/api/order", auth, async (req, res) => {
   try {
     const { cart, totalPrice, address } = req.body;
     let products = [];
